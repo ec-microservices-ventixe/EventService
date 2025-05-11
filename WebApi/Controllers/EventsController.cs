dc08f5974387d9d73d702ca10fbc49c7cff26a7f
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-05-11 20:23:06 +0200

    add validation for jwt bearer token

--- WebApi/Controllers/EventsController.cs
+++ WebApi/Controllers/EventsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebApi.Data.Context;
@@ -13,6 +14,7 @@ public class EventsController(ApplicationDbContext context) : Controller
     private readonly ApplicationDbContext _context = context;
 
     [HttpPost]
+    [Authorize(Roles = "Admin")]
     public IActionResult Create([FromBody] EventEntity entity) {
         if(!ModelState.IsValid) return BadRequest(entity);
 
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebApi.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,14 +12,34 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("EventDbConnection"));
 });
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var secretKey = builder.Configuration["Jwt:SecretKey"];
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = key,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 app.MapOpenApi();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
