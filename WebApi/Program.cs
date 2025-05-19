750c4682c5d18ced8f9174ce5c8cc8185f4b518d
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-05-19 20:34:10 +0200

    add cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,7 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EventDbConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"));
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .WithOrigins("*")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials(); 
+    });
 });
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(options =>
@@ -22,15 +33,15 @@ builder.Services.AddAuthentication(options =>
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
             ValidateIssuerSigningKey = true,
-            ValidateLifetime = false,
+            ValidateLifetime = true,
             ValidateIssuer = false,
             ValidateAudience = false,
             IssuerSigningKeyResolver = (token, SecurityKey, kid, validationParameters) =>
             {
-                using HttpClient httpClient = new();
+                HttpClient httpClient = new();
                 string jwksJson = httpClient.GetStringAsync(builder.Configuration["Jwt:jwksUrl"]).Result;
-                var jwks = new JsonWebKeySet(jwksJson);
-                return jwks.Keys.Where(k => k.Kid == kid).ToList();
+                var keys = new JsonWebKeySet(jwksJson).Keys;
+                return keys.Where(k => k.Kid == kid).ToList();
             }
         };
         options.Events = new JwtBearerEvents
@@ -49,6 +60,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 
